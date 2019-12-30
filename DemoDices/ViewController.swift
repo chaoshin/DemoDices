d668cbd262b41c8ddbd73a958dd4878f2d59e1b5
chaoshin <chao_shin@hotmail.com>
2019-12-30 19:34:20 +0800

    Initial Commit

---
+++ DemoDices/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DemoDices
+//
+//  Created by Chao Shin on 2019/12/30.
+//  Copyright © 2019 Chao Shin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
